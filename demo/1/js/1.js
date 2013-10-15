79b12baac7494b52e501a11356c1dd1e8730189d
mackxu <1004929678@qq.com>
2013-10-15 14:21:08 +0800

    实例化单个星球的视图对象

--- demo/1/js/1.js
+++ demo/1/js/1.js
@@ -9,7 +9,7 @@
 			'delete/:id': 'del'
 		},
 		index: function() {
-			console.log('index');
+			// console.log('index');
 		},
 		add: function() {
 			console.log('add');
@@ -25,4 +25,41 @@
 		del: function(id) {
 			console.log('del');
 		}
+	});
+
+	// 星球的列表视图
+	var WorldList = Backbone.View.extend({
+		
+
+	});
+	// 单个星球视图
+	var WorldView = Backbone.View.extend({
+		initialize: function() {
+			this.render();
+		},
+		render: function() {
+			console.log('WorldView');
+		}
+	});
+
+	// 实例化视图，能自动调用render
+	var w = new WorldView();
+
+	// 星球的模型
+	var World = Backbone.Model.extend({
+		defaults: {
+			name: 'undefined',
+			desc: ''
+		},
+		initialize: function() {
+			this.on('change:name change:desc', this.display, this);
+		},
+		display: function() {
+			console.log(this.get('name'), this.get('desc'));
+		}
+	});
+
+	// 星球的集合
+	var Worlds = Backbone.Collection.extend({
+
 	});
